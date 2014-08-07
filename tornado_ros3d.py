d931225c53057b8e8bf8f3c7754761846cc74b08
Jakub Nowak <jakub.nowak@open-rnd.pl>
2014-08-07 15:36:05 +0200

    Working camera, settings and list all available handlers

--- tornado_ros3d.py
+++ tornado_ros3d.py
@@ -2,11 +2,48 @@ import tornado.ioloop
 import tornado.web
 from tornado.escape import json_encode
 
+settings = {
+        'opacity': 90,
+        'opacitymin': 50,
+        'opacitymax': 100,
+        'opacitystep': 10,
+        'networkhostname': 'wandboard-quad.local',
+        'networkmode': 'static',
+        'networkip': '172.27.0.144',
+        'networkmask': '255.255.255.0',
+        'networkgateway': '0.0.0.0',
+        'networkwifimode': 'client',
+        'networkwifissid': 'wandboard',
+        'networkwifisecurity': 'off',
+        'networkwifikey': 'wand',
+        'networkntpserverip': '11.11.11.11',
+        'networkntpsynchronization': 'off',
+        'language': 'polski',
+}
+
+camera = [
+        {'id': 0, 'name': 'testName0'},
+        {'id': 1, 'name': 'testName1'},
+        {'id': 2, 'name': 'testName2'},
+        {'id': 3, 'name': 'testName3'},
+]
+
+class SettingsHandler(tornado.web.RequestHandler):
+    def get(self):
+        print("SettingsHandler: %s" % json_encode(settings))
+        self.write("%s" % json_encode(settings))
+
+class SettingHandler(tornado.web.RequestHandler):        
+    def get(self, id):
+        print("SettingHandler: %s : %s" % (id, json_encode(settings[id])))
+        temp = { id : settings[id] }
+        self.write("%s" % json_encode(temp))
+
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
         print ("MainHandler: get")
-        #self.write("MainHandler: get")
-        self.write("%s" % ([handler.regex.pattern for handler in self.application.handlers[0][1]])) 
+        handlers = ([handler.regex.pattern for handler in self.application.handlers[0][1]])
+        self.write ("%s" % (json_encode(handlers)))
 
     def post(self):
         print ("MainHandler: post %s" % (self.request.body))
@@ -14,17 +51,14 @@ class MainHandler(tornado.web.RequestHandler):
 
 class CamerasHandler(tornado.web.RequestHandler):
     def get(self):
-        camera = {
-                'name': 'blackmagic',
-                'version': '2.2',
-                }
-        print ("CameraHandler")
-        self.write("CameraHandler: %s" % json_encode(camera))
+        print ("CamerasHandler: %s" % json_encode(camera))
+        self.write("%s" % json_encode(camera))
 
 class CameraHandler(tornado.web.RequestHandler):
     def get(self,id):
-        print ("CameraHandler: get, id: %s" % id)
-        self.write("CameraHandler: get, id: %s" % id)
+        print camera
+        print ("CameraHandler, id: %s, %s" % (id, camera[int(id)]))
+        self.write("%s" % json_encode(camera[int(id)]))
 
 class ServoHandler(tornado.web.RequestHandler):
     def get(self):
@@ -36,6 +70,8 @@ application = tornado.web.Application([
     (r"/camera", CamerasHandler),
     (r"/camera/([0-9]+)", CameraHandler),
     (r"/servo", ServoHandler),
+    (r"/settings", SettingsHandler),
+    (r"/settings/(\w+)", SettingHandler),
 ])
 
 if __name__ == "__main__":
