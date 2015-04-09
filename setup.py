2c9665b1bfab104d78bdefc030d4f7692376877e
Maciej Borzecki <maciej.borzecki@open-rnd.pl>
2015-04-09 15:48:38 +0200

    setup: setuptools support
    Change-Id: Ic1710b851a5579693f128af0846ee728716484a6

---
+++ setup.py
@@ -0,0 +1,54 @@
+#
+# Copyright (c) 2015, Open-RnD Sp. z o.o.  All rights reserved.
+#
+from __future__ import print_function
+from setuptools import setup, find_packages
+import os
+import os.path
+
+NAME='ros3dkrui'
+VERSION = '0.1'
+
+install_requires = ['tornado', 'dbus']
+tests_require = []
+
+ROOT = os.path.dirname(__file__)
+
+def read(fpath):
+    """Load file contents"""
+    with open(os.path.join(ROOT, fpath)) as inf:
+        return inf.read()
+
+def find_data(fpath, prefix=''):
+    def walk_helper(outentries, dirname, files):
+        for fentry in files:
+            full = os.path.join(dirname, fentry)
+            # skip directories
+            if os.path.isdir(full):
+                continue
+            outentries.append((os.path.join(prefix, dirname), [full]))
+
+    outentries = []
+    os.path.walk(fpath, walk_helper, outentries)
+    print ("data files:", outentries)
+    return outentries
+
+
+setup(
+    name=NAME,
+    version=VERSION,
+    packages=find_packages(exclude=['tests', 'tests.*']),
+    description="Ros3D KR Web UI",
+    long_description=read("README.rst"),
+    install_requires=install_requires,
+    tests_require=tests_require,
+    author='OpenRnD',
+    author_email='ros3d@open-rnd.pl',
+    license='closed',
+    entry_points = {
+        'console_scripts': [
+            'ros3d-kr-ui = ros3dkrui.cmd:main'
+        ]
+    },
+    data_files=find_data('web-data', os.path.join('share', NAME))
+)
