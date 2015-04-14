2844bdedb395efd969b79f78d89e2ff1ab65476b
Maciej Borzecki <maciej.borzecki@open-rnd.pl>
2015-04-14 17:14:48 +0200

    cmd: set configuration file path
    Change-Id: Ic090468fff0ee6cfc0eca9f25ff1ca2abb6e9249

--- ros3dkrui/cmd.py
+++ ros3dkrui/cmd.py
@@ -3,6 +3,7 @@
 #
 from __future__ import absolute_import
 from ros3dkrui.web import Application
+from ros3dkrui.system.util import ConfigLoader
 from tornado.ioloop import IOLoop
 import logging
 import argparse
@@ -18,6 +19,11 @@ def parse_arguments():
                         help='HTTP Port, default: {}'.format(DEFAULT_LISTEN_PORT))
     parser.add_argument('-d', '--debug', action='store_true',
                         default=False)
+    parser.add_argument('-c', '--config-path',
+                        help='Configuration file path, ' \
+                        'default: {}'.format(ConfigLoader.CONFIG_PATH),
+                        default=None)
+
     parser.add_argument('document_root', help='Document root')
     return parser.parse_args()
 
@@ -30,9 +36,14 @@ def main():
         level = logging.DEBUG
     logging.basicConfig(level=logging.DEBUG)
 
+    if opts.config_path:
+        ConfigLoader.set_config_location(opts.config_path)
+
     if not opts.document_root:
         logging.error('Document root not provided')
         raise SystemExit(1)
+
+    logging.debug('configuration at: %s', ConfigLoader.CONFIG_PATH)
 
     app = Application(opts.document_root)
     logging.debug('listening on port %d', opts.http_port)
